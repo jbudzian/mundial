74ed447891f781b9d53d370d7f3cc4bef140c206
Jarek Budzianowski <jaroslaw.budzianowski@sollers.pl>
2014-03-31 22:21:38 +0200

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Mundial::Application.routes.draw do
+  get "static_pages/rules"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
