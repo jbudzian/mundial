c47410891e49f88fcbe7eec5c62dde5d62e5a91c
Jarek Budzianowski <jaroslaw.budzianowski@sollers.pl>
2014-04-05 22:03:21 +0200

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Mundial::Application.routes.draw do
   get "static_pages/rules"
+  get "static_pages/home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Mundial Tournament'" do
+      visit '/static_pages/home'
+      page.should have_content('Mundial Tournament')
+    end
+  end
+
   describe "Rules page" do
     it "should have the content 'rules'" do
       visit '/static_pages/rules'
