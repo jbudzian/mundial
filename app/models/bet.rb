3b2c34360cf25843c0a44d4908f1ff0aaa701e9b
Jarek Budzianowski <jaroslaw.budzianowski@sollers.pl>
2014-05-26 23:16:35 +0200

    validation of match start date

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -7,4 +7,14 @@ class Bet < ActiveRecord::Base
   validates :away_team_goals_90mins, :numericality => {:only_integer => true}
   validates :home_team_goals_90mins, :numericality => {:only_integer => true}
   validates :result_90mins, :numericality => {:only_integer => true}
+
+  validate :bet_cannot_be_saved_after_match_start
+  
+  private
+  
+  def bet_cannot_be_saved_after_match_start
+    if match.match_date < Time.now
+      errors[:base] << "This bet cannot be saved because the match has started in the past!"
+    end
+  end
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -6,4 +6,14 @@ class Result < ActiveRecord::Base
   validates :away_team_goals_90mins, :numericality => {:only_integer => true}
   validates :home_team_goals_90mins, :numericality => {:only_integer => true}
   validates :result_full_time, :numericality => {:only_integer => true}
+  
+  validate :result_cannot_be_set_before_match_start
+  
+  private
+  
+  def result_cannot_be_set_before_match_start
+    if match.match_date > Time.now
+      errors[:base] << "This result cannot be saved because the match has not started!"
+    end
+  end
 end
